172894b2ad2b1c1c714997eba9a5abc37029cb94
Aditya Kulkarni <adityaajaykulkarni2000@gmail.com>
2022-06-26 19:43:29 +0530

    change 7
    26/06/22

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
+import { motion } from 'framer-motion'
+
 const styles = {
     active_link : "block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white", 
     link : "block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
@@ -14,7 +16,10 @@ function Navbar({currentLink}) {
   <div className="container flex flex-wrap justify-between items-center mx-auto">
    <Link href={"/home"}>
     <a className="flex items-center">
-        <Image src="/logo.jpg" width='60' height={60}/><span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white" >Crest Homeo Clinic</span> 
+        <Image src="/logo.jpg" width='60' height={60}/>
+        <motion.span animate={{color :['#4C3A51', '#774360', '#B25068', '#E7AB79']}} transition={{repeat :Infinity, delay:1.5}}>
+          <span className="self-center text-2xl font-bold font-serif whitespace-nowrap dark:text-white" >Crest Homeo Clinic</span> 
+        </motion.span>
     </a>
     </Link>
     <button data-collapse-toggle="mobile-menu" type="button" className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mobile-menu" aria-expanded="false">
--- pages/gallery.js
+++ pages/gallery.js
@@ -7,7 +7,7 @@ const imageArray = ["gallery/1.jpeg", "gallery/2.jpeg","gallery/3.jpeg","gallery
 
 function Gallery() {
     return (
-        <div className='container mx-auto px-4 py-4'>
+        <div className='container mx-auto px-4 pb-4'>
             <Navbar currentLink={"Gallery"} />
             <div className="grid grid-cols-4 gap-4">
                 {
