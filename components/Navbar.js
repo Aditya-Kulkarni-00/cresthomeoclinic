eff0db2e9d2dc1179061e4eb6da4b014df8e1f65
Aditya Kulkarni <adityaajaykulkarni2000@gmail.com>
2022-06-26 18:48:00 +0530

    chnage 3
    26/06/22

--- components/Navbar.js
+++ components/Navbar.js
@@ -12,9 +12,11 @@ function Navbar({currentLink}) {
   return (
 <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800">
   <div className="container flex flex-wrap justify-between items-center mx-auto">
-    <a href="/" className="flex items-center">
+   <Link href={"/home"}>
+    <a className="flex items-center">
         <Image src="/logo.jpg" width='60' height={60}/><span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white" >Crest Homeo Clinic</span> 
     </a>
+    </Link>
     <button data-collapse-toggle="mobile-menu" type="button" className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mobile-menu" aria-expanded="false">
       <span className="sr-only">Open main menu</span>
       <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
@@ -23,7 +25,7 @@ function Navbar({currentLink}) {
     <div className="hidden w-full md:block md:w-auto" id="mobile-menu">
       <ul className="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
           {linksArray.map((link , index, arr) => (
-            <Link href={`/${link.toLowerCase()}`}>
+            <Link href={`/${link.toLowerCase()}`} key={index}>
               <a className={`${link == currentLink ? styles.active_link : styles.link}`}>{link == "Social" ? `${link} Responsiblity` : 
                 link == "Customized" ? `${link} Programs` : link}</a>
             </Link>
