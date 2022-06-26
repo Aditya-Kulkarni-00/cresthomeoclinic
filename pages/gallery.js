54d45dad052c2d00c74cd7708321e7a8d4994754
Aditya Kulkarni <adityaajaykulkarni2000@gmail.com>
2022-06-26 19:49:25 +0530

    change 8
    26/06/22

--- pages/gallery.js
+++ pages/gallery.js
@@ -2,8 +2,7 @@ import Image from 'next/image';
 import React from 'react'
 import Navbar from '../components/Navbar';
 
-const imageArray = ["gallery/1.jpeg", "gallery/2.jpeg","gallery/3.jpeg","gallery/4.jpeg","gallery/5.jpeg","gallery/6.jpeg",
-"gallery/7.jpeg","gallery/8.jpeg","gallery/9.jpeg","gallery/10.jpeg","gallery/11.jpeg","gallery/12.jpeg"]
+const imageArray = ["gallery/1.jpeg", "gallery/2.jpeg","gallery/3.jpeg","gallery/4.jpeg","gallery/5.jpeg"]
 
 function Gallery() {
     return (
--- pages/social.js
+++ pages/social.js
@@ -1,10 +1,21 @@
+import Image from 'next/image';
 import React from 'react'
 import Navbar from '../components/Navbar';
 
+const imageArray = ["social/1.jpeg", "social/2.jpeg","social/3.jpeg","social/4.jpeg","social/5.jpeg","social/6.jpeg",
+"social/7.jpeg","social/8.jpeg","social/9.jpeg","social/10.jpeg","social/11.jpeg","social/12.jpeg"]
+
 function Social() {
   return (
-    <div>
+    <div className='container mx-auto px-4 mb-4'>
         <Navbar currentLink={"Social"}/>
+        <div className="grid grid-cols-4 gap-4">
+                {
+                    imageArray.map((img, index)=>(
+                        <Image src={`/${img}`} width={300} height={300} className={`rounded-md`} key={index}/>
+                    ))
+                }
+            </div>
     </div>
   )
 }
