2788db5cbe602f7cd6b282a5e87f1b2eaca24bc8
Aditya Kulkarni <adityaajaykulkarni2000@gmail.com>
2022-06-28 23:35:59 +0530

    change 21
    28/06/22

--- pages/treatment.js
+++ pages/treatment.js
@@ -12,28 +12,29 @@ function Treatment() {
         <Image src={"/treatment/1.jpg"} width={600} height={600} className="rounded-lg"/>
         <div className='w-full md:w-1/2 mt-6 md:ml-6 md:mt-0 text-2xl px-5 leading-loose  mr-4 flex-none'>
           <p className=''>
-          <span className='text-black text-3xl font-semibold'>HOMEOPATHIC TREATMENTS FOR </span>ULCERS,
-          <span className='text-sky-500'>TONSILLITIS,</span>
-          <span className='text-rose-500'>EPILEPSY,<br/></span>
-          <span className='text-violet-500'>LUNG DISEASE,</span>
-          <span className='text-green-500'>ECZEMA,</span>
-          <span className='text-orange-400'>INFERTILITY,<br/></span>
-          <span className='text-sky-500'>DIGESTIVE PROBLEMS,</span>
-          <span className='text-rose-500'>PIMPLES,</span>
-          <span className='text-violet-500'>HAIR LOSS,</span>
-          <span className='text-green-500'>ASTHMA,</span>
-          <span className='text-orange-400'>OSTEOARTHRITIS,<br/></span>
-          <span className='text-sky-500'>HYPERACIDITY,</span>
-          <span className='text-rose-500'>FISSURE,</span>
-          <span className='text-violet-500'>CORN REMOVAL,</span>
-          <span className='text-green-500'>PARKINSON DISEASE,</span>
-          <span className='text-orange-500'>PSORIASIS,</span>
-          <span className='text-sky-500'>TINNITUS,<br/></span>
-          <span className='text-black'>PEPTIC ULCER,</span>
-          <span className='text-rose-500'>GASTROENTERITIS,<br/></span>
-          <span className='text-violet-500'>TYPHOID, PILES,</span>
-          <span className='text-orange-500'>KIDNEY STONE,</span>
-          <span className='text-sky-500'>POLYCYSTIC OVARIAN SYNDROME AND MORE..</span>
+          <span className='text-black text-3xl font-semibold'>HOMEOPATHIC TREATMENTS FOR </span>
+          <span>ULCERS, </span>
+          <span className='text-sky-500'>TONSILLITIS, </span>
+          <span className='text-rose-500'>EPILEPSY, <br/></span>
+          <span className='text-violet-500'>LUNG DISEASE, </span>
+          <span className='text-green-500'>ECZEMA, </span>
+          <span className='text-orange-400'>INFERTILITY, <br/></span>
+          <span className='text-sky-500'>DIGESTIVE PROBLEMS, </span>
+          <span className='text-rose-500'>PIMPLES, </span>
+          <span className='text-violet-500'>HAIR LOSS, </span>
+          <span className='text-green-500'>ASTHMA, </span>
+          <span className='text-orange-400'>OSTEOARTHRITIS, <br/></span>
+          <span className='text-sky-500'>HYPERACIDITY, </span>
+          <span className='text-rose-500'>FISSURE, </span>
+          <span className='text-violet-500'>CORN REMOVAL, </span>
+          <span className='text-green-500'>PARKINSON DISEASE, </span>
+          <span className='text-orange-500'>PSORIASIS, </span>
+          <span className='text-sky-500'>TINNITUS, <br/></span>
+          <span className='text-black'>PEPTIC ULCER, </span>
+          <span className='text-rose-500'>GASTROENTERITIS, <br/></span>
+          <span className='text-violet-500'>TYPHOID, PILES, </span>
+          <span className='text-orange-500'>KIDNEY STONE, </span>
+          <span className='text-sky-500'>POLYCYSTIC OVARIAN SYNDROME AND MORE.. </span>
           </p>
         </div>
       </div>
