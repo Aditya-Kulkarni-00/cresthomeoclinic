6816e69895356379f079c2dcf780f1c85b34e44c
Aditya Kulkarni <adityaajaykulkarni2000@gmail.com>
2022-06-29 19:16:49 +0530

    change 25
    29/06/22

--- components/BranchDetails.js
+++ components/BranchDetails.js
@@ -9,6 +9,8 @@ function BranchDetails() {
           <p className='text-gray-600'>Sri Kalki Chambers, Oppo. Pullareddy Sweets </p>
           <p className='text-gray-600'>Miyapur , Hyderabad, Pin 500050</p>
           <p className='text-gray-600'>State : Telangana , India</p>
+          <p className='text-gray-600'>Phone : 8886911148</p>
+          <p className='text-gray-600'>Phone : 8886911147</p>
         </div>
         <div className='md:w-1/2 mt-4 md:mt-0 md:flex flex-col items-end justify-end text-justify'>
           <h2 className='font-semibold text-xl mb-4 text-sky-400'>Branch 2: HHW Clinic</h2>
@@ -16,6 +18,8 @@ function BranchDetails() {
           <p className='text-gray-600'>Gannavaram , City Vijaywada</p>
           <p className='text-gray-600'>Dist : Krishna , Pin 521101</p>
           <p className='text-gray-600'>State : Andhra Pradesh, India</p>
+          <p className='text-gray-600'>Phone : 9966450446</p>
+          <p className='text-gray-600'>Phone : 8331890446</p>
         </div>
 
     </div>
