41cafde3853860a6c209a17e5b5d08e8acebe21a
Aditya Kulkarni <adityaajaykulkarni2000@gmail.com>
2022-07-03 14:12:57 +0530

    change 28
    03/07/22

--- components/Program.js
+++ components/Program.js
@@ -5,8 +5,8 @@ function Program({title , tiles}) {
   return (
     <div className='border rounded-md py-4 px-4 bg-slate-300'>
         <button onClick={()=> setDropdown(!dropdown)} className={`w-full flex flex-row justify-start items-center ${dropdown && 'mb-4'}`}>{title}</button>
-        {dropdown &&  tiles.map((tile)=>(
-            <p>{tile}</p>
+        {dropdown &&  tiles.map((tile ,i)=>(
+            <p key={i}>{tile}</p>
         ))}
     </div>
   )
